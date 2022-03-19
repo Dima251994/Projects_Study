b139812a6470dc8c1ef000c73b3107189d2536be
Dima <73307460+Dima251994@users.noreply.github.com>
2022-03-19 20:04:34 +0300

    New branch info

--- Number_game.py
+++ Number_game.py
@@ -3,4 +3,8 @@ import random
 number_from_random = random.randint(0,6)
 user_number = input("Enter number: ")
 
+def qusseing(number_from_user):
+    print("Number from user is", number_from_user)
+    
+
 
