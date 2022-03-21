db67f0aa45ebd1ec59ccb21e26bc4119c1df644f
Dima <73307460+Dima251994@users.noreply.github.com>
2022-03-21 03:15:24 +0300

    Finish program about guessing numbers

--- Number_game.py
+++ Number_game.py
@@ -1,10 +1,37 @@
+#Program for guessing number 
 import random
 
-number_from_random = random.randint(0,6)
-user_number = input("Enter number: ")
+def main():
+    play_again = "yes"
+    while play_again == "yes":
+        if play_again != "yes":
+            break
+        number_from_random = random.randint(0,11)
+        tries = 3
+        while tries > 0:
+            user_number = int(input("Enter number: "))
+            if number_from_random == user_number:
+                print("You win")
+                play_again = input("Play again? ")
+            elif number_from_random > user_number:
+                print("Слишком маленькое число")
+                tries -= 1
+            elif number_from_random < user_number:
+                print("Слишком большое число")
+                tries -= 1
+        else:
+            print("You lose")
+            play_again = input("Play again? ")
+    
 
-def qusseing(number_from_user):
-    print("Number from user is", number_from_user)
+
+#def play_again(again):
+ ##      main()
+    
+#play_again(input("Play?").lower())
+main()
+
+
     
 
 
