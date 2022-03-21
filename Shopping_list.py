bf6d0f4c3fd9af335ac2e15ea1e12abd18d3913a
Dima <73307460+Dima251994@users.noreply.github.com>
2022-03-21 16:21:27 +0300

    Finish program about shopping list

---
+++ Shopping_list.py
@@ -0,0 +1,50 @@
+# Программа для добавления товаров в список покупок
+
+buying_list = []
+
+
+def add():
+    stuff = "yes"
+    while True:
+        if stuff == "no" or stuff == "NO":
+            break
+        else:
+            print("Input 'no' for stop adding")
+            #stuff = input("Enter stuff: ")
+            buying_list.append(stuff)
+            stuff = input("Enter stuff: ")
+            
+
+def remove():
+    stuff = "yes"
+    while True:
+        if stuff == "no" or stuff == "No":
+            break
+        else:
+            buying_list.remove(stuff)
+            print("Remove item more ? ")
+            stuff = input("Enter what remove: ")
+
+
+def show():
+    print("Items in list: ", end="")
+    for item in buying_list:
+        print(item, sep=",")
+    print( )
+        
+
+def main():
+    while True:
+        command = input("Enter command for work with list: ")
+        if command == "add":
+            add()
+        elif command == "remove":
+            remove()
+        elif command == "show":
+            show()
+        else:
+            break
+
+main()
+        
+        
