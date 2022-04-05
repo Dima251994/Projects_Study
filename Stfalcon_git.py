7c1e68b87fc1e0763e6b3e13367068e800c0c5f7
Dima <73307460+Dima251994@users.noreply.github.com>
2022-04-06 01:56:13 +0300

    Add binary search function

--- Stfalcon_git.py
+++ Stfalcon_git.py
@@ -1,2 +1,21 @@
-# StFalcon Git lection
-print("New \' world")
+my_list = [4,5,6,7,8,9,10,11,12]
+
+def find_index(work_list, number):
+    min_index = 0
+    max_index = len(work_list) - 1 
+    while min_index <= max_index:
+        mid_index = int((min_index + max_index)/2)
+        list_number = work_list[mid_index]
+        if list_number == number :
+            print(mid_index)
+            return mid_index
+        if list_number > number:
+            max_index = mid_index - 1
+        else:
+            min_index = mid_index + 1
+    print("Dont have this number")
+            
+
+
+
+
