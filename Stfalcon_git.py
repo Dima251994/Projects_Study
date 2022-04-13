a9165a55a77e8c165842706bf0499fd28914f7b0
Dima <73307460+Dima251994@users.noreply.github.com>
2022-04-14 00:32:15 +0300

    New commit

--- Stfalcon_git.py
+++ Stfalcon_git.py
@@ -16,32 +16,11 @@ def find_index(work_list, number):
         else:
             min_index = mid_index + 1
     print("Dont have this number")
-"""            
-
-"""
-class Car():
-    def __init__(self, brand, year):
-        self.brand_of_car = brand
-        self.year_of_car = year
-
-    def change_year(self, new_year):
-        self.year_of_car = new_year
-
-    def show_info(self):
-        print(self.brand_of_car, self.year_of_car)
-
-car_1 = Car("Skoda", 1987)
-car_2 = Car("Mazda", 1999)
-car_3 = Car("Honda", 2003)
-
-cars = [car_1, car_2, car_3]
-
-cars[0].show_info()
-"""
+"""           
 
 
 
-my_list = [3,1,2,0]
 
-my_list.reverse()
-print(my_list)
+
+
+
