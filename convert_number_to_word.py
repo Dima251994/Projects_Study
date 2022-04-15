047c3436d03d7ac5f8648297ab313875ef53a14e
Dima <73307460+Dima251994@users.noreply.github.com>
2022-04-16 00:41:02 +0300

    Ended convert program

--- convert_number_to_word.py
+++ convert_number_to_word.py
@@ -32,41 +32,50 @@ two_number = {
 
 def for_two_numbers_func(user_number):
     # Function for two numbers from user
-    second_number_from = user_number%10
+    singular_number = user_number%10
     decimal_number = (user_number // 10) * 10
     for key in two_number.keys():
         if decimal_number == key:
             list_for_two_numbers.append(two_number[key])
     for key in one_number.keys():
-        if second_number_from == key:
+        if singular_number == key:
             list_for_two_numbers.append(one_number[key])
 
 
 list_for_two_numbers = []
-number_from_user = int(input("Enter number for convert: "))
-converted_number = str(number_from_user)
-if len(converted_number) == 1:
-    for key in one_number.keys():
-        if number_from_user == key:
-            final_one_number = one_number[key]
-            print(final_one_number)
-elif len(converted_number) == 2:
-    for_two_numbers_func(number_from_user)
-elif len(converted_number) == 3:
-    number_with_three = number_from_user // 100
-    two_numbers_get = number_from_user % 100
-    for key in one_number.keys():
-        if number_with_three == key:
-            list_for_two_numbers.append(one_number[key])
-            list_for_two_numbers.append("Hundred")
-    for_two_numbers_func(two_numbers_get)
-
-else:
-    print("No number")  
-
-
-if list_for_two_numbers:
-    for word in list_for_two_numbers:
-        print(word, end=" ")
+try: # Check if not number 
+    number_from_user = int(input("Enter number for convert: "))
+    converted_number = str(number_from_user)
+    if len(converted_number) == 1: # work if only one number
+        for key in one_number.keys():
+            if number_from_user == key:
+                list_for_two_numbers.append(one_number[key])
+    elif number_from_user >= 10 and number_from_user <=20:
+        for key in two_number.keys():
+            if number_from_user == key:
+                list_for_two_numbers.append(two_number[key])
+    elif len(converted_number) == 2:
+        for_two_numbers_func(number_from_user)
+    elif len(converted_number) == 3:
+        number_with_three = number_from_user // 100
+        two_numbers_get = number_from_user % 100
+        if two_numbers_get >= 10 and two_numbers_get <=20:
+            for key in two_number.keys():
+                if two_numbers_get == key:
+                    list_for_two_numbers.append(one_number[number_with_three])
+                    list_for_two_numbers.append("Hundred")
+                    list_for_two_numbers.append(two_number[key])
+        else:
+            for key in one_number.keys():
+                if number_with_three == key:
+                    list_for_two_numbers.append(one_number[key])
+                    list_for_two_numbers.append("Hundred")
+                    for_two_numbers_func(two_numbers_get)
+    else:
+        print("No number")  
+except:
+    print("Enter number")
+for word in list_for_two_numbers:
+    print(word, end=" ")
 
     
